31675efab6a2a1bc7e16db5c600370603c54fb7a
pajogusi <pajogusi.uk@gmail.com>
2026-06-07 11:08:10 +0100

    Update app version from 6.4 to 6.5

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const APP_CONFIG = {
-  version: "6.4",
+  version: "6.5",
   siteName: "Enciclopédia de IAs",
   subtitle: "Links, descrição e classificação prática de 0 a 5 estrelas.",
   footerText: "Classificação prática e subjetiva.",
