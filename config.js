8d8fbb3351db561f4171661e703551c7a3cb8b11
pajogusi <pajogusi.uk@gmail.com>
2026-06-07 10:41:09 +0100

    add config file

---
+++ config.js
@@ -0,0 +1,7 @@
+const APP_CONFIG = {
+  version: "6.3",
+  siteName: "Enciclopédia de IAs",
+  subtitle: "Links, descrição e classificação prática de 0 a 5 estrelas.",
+  footerText: "Classificação prática e subjetiva.",
+  blogUrl: ""
+};
